02fc88c99f7a7bbaabe7248cffd275b347a80c8e
Eric Pait <ericpait@gmail.com>
2014-12-23 14:16:45 -0500

    Updated markup of blog page to reflect YMCAYAG

--- wp-content/themes/YPress/index.php
+++ wp-content/themes/YPress/index.php
@@ -9,29 +9,25 @@
     			}?>
 			</div>
   			<div id="bc_spacer"></div>
-  			<div id="bc_right"><a href="/news">Blog</a></div>
+  			<div id="bc_right"><a href="/news">News</a></div>
   		</div>
 	</div>
 </div>
 <div class="row-fluid">
-	<div class="col-sm-8">
-		<div class="homeTitle"><h4>KY YMCA Blog</h4></div>
-		<hr>
-		<div class="main-content">
-			<?php query_posts('posts_per_page=5&cat=17'); ?>
+	<div class="col-sm-8 homepageBlog">
+		<div class="homeTitle"><h4>Program News</h4></div>
+		<hr class="">
+		<div class="posts-wrapper">
+			<?php query_posts('posts_per_page=5&cat=-2'); ?>
 		    <?php if (have_posts()) : ?>
 		    <?php while (have_posts()) : the_post(); ?>    
 		        <div class="post">
-	                <div class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-	                <div class="post-author">By <a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>"><?php the_author_meta('display_name'); ?></a> on  <?php the_time(get_option('date_format')); ?></div>
-	                <div class="excerpt"><?php the_excerpt(); ?></div>
-	                <a class="black_arrow" href="<?php the_permalink(); ?>">READ MORE</a>
+			                <div class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+			                <div class="post-author">By <a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>"><?php the_author_meta('display_name'); ?></a> on  <?php the_time(get_option('date_format')); ?></div>
+			                <div class="excerpt"><?php the_excerpt(); ?></div>
+			                <a class="black_arrow" href="<?php the_permalink(); ?>">READ MORE</a>
 		        </div> 
 		    <?php endwhile; ?>
-		    <!-- Pagination -->
-				<div <?php post_class('pagination'); ?>>
-				<?php if(function_exists('wp_paginate')) { wp_paginate(); } ?>
-				</div>
 		    <?php endif; ?>
 		</div>
 	</div>
