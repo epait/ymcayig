c91e3fad8fb5745b36700bb6047da9c905f2a3d7
Eric Pait <ericpait@gmail.com>
2014-12-24 22:00:03 -0500

    Created duplicate page template to turn into SVG map of US

---
+++ wp-content/themes/YPress/page-map.php
@@ -0,0 +1,41 @@
+<?php get_header() ?>
+<!-- Main Content -->
+        <div class="row-fluid">
+        	<div class="col-sm-12">
+	            <div class="single-content">
+	            	<?php if (have_posts()) : ?>
+	            		<?php while (have_posts()) : the_post(); ?>
+	            			<div class="headerImage"><?php the_post_thumbnail('full', array('class' => 'img-responsive')); ?></div>
+	            			<div id="breadcrumb">
+		  						<div id="bc_left">
+		  							<?php if(function_exists('bcn_display')) {
+		        						bcn_display();
+		    						}?>
+								</div>
+		  						<div id="bc_spacer"></div>
+		  						<div id="bc_right"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+		  					</div>
+	            			<div class="pageTitle"><?php the_title(); ?></div>
+	            			<!-- Map goes here -->
+							<?php the_content() ?>
+							<?php
+								// check if the repeater field has rows of data
+								if( have_rows('page_sections') ): ?>
+									<br />
+									<?php 
+								 	// loop through the rows of data
+								    while ( have_rows('page_sections') ) : the_row();
+								        // display a sub field value
+								        the_sub_field('additional_section');
+								        ?> <br /> <?php 
+								    endwhile;
+								else :
+								    // no rows found
+								endif;
+							?>
+	            		<?php endwhile; ?>
+	            	<?php endif; ?>
+	            </div>
+        	</div>
+        </div>
+<?php get_footer() ?>
